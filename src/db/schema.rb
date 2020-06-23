2bc2a8c2d989b4536f5a9816691be62eba6e15c6
Côme Wasik <wasik.come@gmail.com>
2020-06-23 13:31:30 +0200

    repare branch

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_06_20_192341) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
