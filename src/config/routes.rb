08356194567acd7990c97473e2c007215f55d6eb
Côme Wasik <wasik.come@gmail.com>
2020-06-23 13:05:28 +0200

    delete post and comment

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
       registrations: 'users/registrations'
   }
-  resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :posts
   resources :films
 end
